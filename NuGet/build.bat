3c3524c52a45a9db4bdd28352713929379544b1d
Tatarincev <et@virtoway.com>
2016-09-26 14:07:14 +0200

    2.12.0

--- NuGet/build.bat
+++ NuGet/build.bat
@@ -8,7 +8,7 @@ IF NOT DEFINED MSBUILD_PATH SET MSBUILD_PATH=%WINDIR%\Microsoft.NET\Framework\v4
 
 "%MSBUILD_PATH%" "%SOURCE_DIR%\VirtoCommerce.ContentModule.sln" /nologo /verbosity:m /t:Build /p:Configuration=Release;Platform="Any CPU"
 
-nuget pack "%SOURCE_DIR%\VirtoCommerce.ContentModule.Client\VirtoCommerce.ContentModule.Client.csproj" -IncludeReferencedProjects -Symbols -Properties Configuration=Release -o "%TARGET_DIR%"
+
 nuget pack "%SOURCE_DIR%\VirtoCommerce.ContentModule.Data\VirtoCommerce.ContentModule.Data.csproj" -IncludeReferencedProjects -Symbols -Properties Configuration=Release -o "%TARGET_DIR%"
 
 @pause
