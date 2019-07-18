d71690c2b2b2ef60b21e8c7f22fe5c4ad881e8de
Basil Kotov <basilkot@gmail.com>
2019-07-18 15:40:28 +0200

    code format

--- VirtoCommerce.ContentModule.Web/Scripts/content.js
+++ VirtoCommerce.ContentModule.Web/Scripts/content.js
@@ -1,4 +1,4 @@
-﻿//Call this to register our module to main application
+//Call this to register our module to main application
 var moduleName = "virtoCommerce.contentModule";
 
 if (AppDependencies != undefined) {
@@ -6,7 +6,9 @@ if (AppDependencies != undefined) {
 }
 
 angular.module(moduleName, [])
-    .run(['platformWebApp.authService', 'platformWebApp.mainMenuService', 'platformWebApp.widgetService', '$state', 'platformWebApp.bladeNavigationService', 'platformWebApp.permissionScopeResolver', 'virtoCommerce.storeModule.stores', 'virtoCommerce.contentModule.menuLinkList-associationTypesService',
+    .run(['platformWebApp.authService', 'platformWebApp.mainMenuService', 'platformWebApp.widgetService', '$state',
+        'platformWebApp.bladeNavigationService', 'platformWebApp.permissionScopeResolver', 'virtoCommerce.storeModule.stores',
+        'virtoCommerce.contentModule.menuLinkList-associationTypesService',
         function (authService, mainMenuService, widgetService, $state, bladeNavigationService, scopeResolver, stores, associationTypesService) {
 
             var menuItem = {
