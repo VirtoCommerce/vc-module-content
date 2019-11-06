dba29250dd2e23cbec7959c7d4e9f7377475b8f9
Andrey Kozlov <yecli.github@gmail.com>
2019-11-06 15:51:15 +0200

    Added KnownDocumentTypes

---
+++ VirtoCommerce.ContentModule.Data/Search/ContentKnownDocumentTypes.cs
@@ -0,0 +1,7 @@
+namespace VirtoCommerce.ContentModule.Data.Search
+{
+    public static class ContentKnownDocumentTypes
+    {
+        public const string MarkdownPages = "Markdown content pages";
+    }
+}
