6c55e80fc064b75c16b484b56c8a243d9aceb8a9
Andrei Kolchanov <akak1977@gmail.com>
2020-07-29 09:04:08 +0500

    VP-3266: Change ContentCacheRegion to support new cache expiration technique

--- src/VirtoCommerce.ContentModule.Data/Caching/ContentCacheRegion.cs
+++ src/VirtoCommerce.ContentModule.Data/Caching/ContentCacheRegion.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections.Concurrent;
-using System.Threading;
 using Microsoft.Extensions.Primitives;
 using VirtoCommerce.Platform.Core.Caching;
 
@@ -8,23 +6,20 @@ namespace VirtoCommerce.ContentModule.Data.Model
 {
     public class ContentCacheRegion : CancellableCacheRegion<ContentCacheRegion>
     {
-        private static readonly ConcurrentDictionary<string, CancellationTokenSource> _dirRegionTokenLookup = new ConcurrentDictionary<string, CancellationTokenSource>();
-
         public static IChangeToken CreateChangeToken(string contentStore)
         {
             if (contentStore == null)
             {
                 throw new ArgumentNullException(nameof(contentStore));
             }
-            var cancellationTokenSource = _dirRegionTokenLookup.GetOrAdd(contentStore, new CancellationTokenSource());
-            return new CompositeChangeToken(new[] { CreateChangeToken(), new CancellationChangeToken(cancellationTokenSource.Token) });
+            return CreateChangeTokenForKey(contentStore);
         }
 
         public static void ExpireContent(string contentStore)
         {
-            if (_dirRegionTokenLookup.TryRemove(contentStore, out var token))
+            if (contentStore != null)
             {
-                token.Cancel();
+                ExpireTokenForKey(contentStore);
             }
         }
     }
