787bc8c86565833ef95764c61f23e1f712f8321c
Andrei Kolchanov <akak1977@gmail.com>
2021-09-02 15:57:17 +0500

    PT-3725: Bump platform up to 3.76 to deliver PR-3724

--- src/VirtoCommerce.ContentModule.FileSystem/FileSystemContentBlobStorageProvider.cs
+++ src/VirtoCommerce.ContentModule.FileSystem/FileSystemContentBlobStorageProvider.cs
@@ -1,13 +1,15 @@
 using Microsoft.Extensions.Options;
 using VirtoCommerce.ContentModule.Core.Services;
 using VirtoCommerce.Platform.Assets.FileSystem;
+using VirtoCommerce.Platform.Core;
+using VirtoCommerce.Platform.Core.Settings;
 
 namespace VirtoCommerce.ContentModule.FileSystem
 {
     public class FileSystemContentBlobStorageProvider : FileSystemBlobProvider, IBlobContentStorageProvider
     {
-        public FileSystemContentBlobStorageProvider(IOptions<FileSystemContentBlobOptions> options)
-            : base(options)
+        public FileSystemContentBlobStorageProvider(IOptions<FileSystemContentBlobOptions> options, IOptions<PlatformOptions> platformOptions, ISettingsManager settingsManager)
+            : base(options, platformOptions, settingsManager)
         {
         }
     }
