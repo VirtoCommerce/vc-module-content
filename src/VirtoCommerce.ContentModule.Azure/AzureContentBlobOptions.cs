9dc841767868a4b7d20a98f547f85901c123a431
basilkot <basilkot@gmail.com>
2023-09-12 08:16:06 +0200

    remove rootpath option

--- src/VirtoCommerce.ContentModule.Azure/AzureContentBlobOptions.cs
+++ src/VirtoCommerce.ContentModule.Azure/AzureContentBlobOptions.cs
@@ -1,14 +1,10 @@
 using System;
-using System.ComponentModel.DataAnnotations;
 using VirtoCommerce.AzureBlobAssetsModule.Core;
 
 namespace VirtoCommerce.ContentModule.Azure
 {
     public class AzureContentBlobOptions : AzureBlobOptions, ICloneable
     {
-        [Required]
-        public string RootPath { get; set; }
-
         public object Clone()
         {
             return MemberwiseClone();
--- src/VirtoCommerce.ContentModule.Azure/AzureContentBlobStorageProvider.cs
+++ src/VirtoCommerce.ContentModule.Azure/AzureContentBlobStorageProvider.cs
@@ -1,14 +1,9 @@
-using System;
-using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Options;
 using VirtoCommerce.AssetsModule.Core.Assets;
 using VirtoCommerce.AzureBlobAssetsModule.Core;
 using VirtoCommerce.ContentModule.Core.Services;
 using VirtoCommerce.Platform.Core;
-using VirtoCommerce.Platform.Core.Common;
-using VirtoCommerce.Platform.Core.Extensions;
 using VirtoCommerce.Platform.Core.Settings;
 
 namespace VirtoCommerce.ContentModule.Azure
@@ -22,72 +17,9 @@ namespace VirtoCommerce.ContentModule.Azure
             _options = options.Value;
         }
 
-        public override Stream OpenRead(string url)
-        {
-            return base.OpenRead(NormalizeUrl(url));
-        }
-
-        public override Task<BlobInfo> GetBlobInfoAsync(string url)
-        {
-            return base.GetBlobInfoAsync(NormalizeUrl(url));
-        }
-
-        public override Task CreateFolderAsync(BlobFolder folder)
-        {
-            if (folder == null)
-                throw new ArgumentNullException(nameof(folder));
-
-            if (folder.ParentUrl.IsNullOrEmpty())
-            {
-                folder.Name = NormalizeUrl(folder.Name);
-            }
-            return base.CreateFolderAsync(folder);
-        }
-
-        public override Stream OpenWrite(string url)
-        {
-            return base.OpenWrite(NormalizeUrl(url));
-        }
-
-        public override Task RemoveAsync(string[] urls)
-        {
-            urls = urls.Select(NormalizeUrl).ToArray();
-
-            return base.RemoveAsync(urls);
-        }
-
         public override async Task<BlobEntrySearchResult> SearchAsync(string folderUrl, string keyword)
         {
-            folderUrl = NormalizeUrl(folderUrl);
-
             var result = await base.SearchAsync(folderUrl, keyword);
-
-            var rootAzurePath = _options.RootPath.Replace('\\', '/');
-
-            foreach (var blobEntry in result.Results)
-            {
-                blobEntry.RelativeUrl = blobEntry.RelativeUrl.Replace($"/{rootAzurePath}", string.Empty);
-            }
-
-            return result;
-        }
-
-        /// <summary>
-        /// Chroot url (artificial add parent 'chroot' folder)
-        /// </summary>
-        /// <param name="url"></param>
-        /// <returns></returns>
-        private string NormalizeUrl(string url)
-        {
-            var result = _options.RootPath;
-            if (!string.IsNullOrEmpty(url))
-            {
-                if (url.IsAbsoluteUrl())
-                {
-                    url = Uri.UnescapeDataString(new Uri(url).AbsolutePath);
-                }
-                result = UrlHelperExtensions.Combine(_options.RootPath, url.Replace(_options.RootPath, string.Empty));
-            }
             return result;
         }
     }
