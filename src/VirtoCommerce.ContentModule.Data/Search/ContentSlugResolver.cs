34541f548a9ddc55645ebd39522ccaeff465bc0e
basilkot <basilkot@gmail.com>
2023-10-27 12:28:08 +0100

    add namespace for extension methods

--- src/VirtoCommerce.ContentModule.Data/Search/ContentSlugResolver.cs
+++ src/VirtoCommerce.ContentModule.Data/Search/ContentSlugResolver.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
+using VirtoCommerce.ContentModule.Core.Extensions;
 using VirtoCommerce.ContentModule.Core.Model;
 using VirtoCommerce.ContentModule.Core.Search;
 using VirtoCommerce.CoreModule.Core.Seo;
