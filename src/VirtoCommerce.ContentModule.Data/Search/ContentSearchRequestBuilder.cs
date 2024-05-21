f65817487450847039c9dbf0d06360c0207e4d9c
Basil Kotov <basilkot@gmail.com>
2024-05-21 11:41:41 +0200

    VCST-1110, VCST-690: add language to index document model (#176)

--- src/VirtoCommerce.ContentModule.Data/Search/ContentSearchRequestBuilder.cs
+++ src/VirtoCommerce.ContentModule.Data/Search/ContentSearchRequestBuilder.cs
@@ -6,21 +6,24 @@ using VirtoCommerce.Platform.Core.Common;
 using VirtoCommerce.SearchModule.Core.Extensions;
 using VirtoCommerce.SearchModule.Core.Model;
 using VirtoCommerce.SearchModule.Core.Services;
+using VirtoCommerce.StoreModule.Core.Services;
 
 namespace VirtoCommerce.ContentModule.Data.Search
 {
     public class ContentSearchRequestBuilder : ISearchRequestBuilder
     {
         private readonly ISearchPhraseParser _searchPhraseParser;
+        private readonly IStoreService _storeService;
 
         public virtual string DocumentType => FullTextContentSearchService.ContentDocumentType;
 
-        public ContentSearchRequestBuilder(ISearchPhraseParser searchPhraseParser)
+        public ContentSearchRequestBuilder(ISearchPhraseParser searchPhraseParser, IStoreService storeService)
         {
             _searchPhraseParser = searchPhraseParser;
+            _storeService = storeService;
         }
 
-        public virtual Task<SearchRequest> BuildRequestAsync(SearchCriteriaBase criteria)
+        public virtual async Task<SearchRequest> BuildRequestAsync(SearchCriteriaBase criteria)
         {
             SearchRequest result = null;
 
@@ -28,22 +31,23 @@ namespace VirtoCommerce.ContentModule.Data.Search
             {
                 // GetFilters() modifies Keyword
                 searchCriteria = searchCriteria.CloneTyped();
-                var filters = GetFilters(searchCriteria);
+                var filters = await GetFilters(searchCriteria);
 
                 result = new SearchRequest
                 {
                     SearchKeywords = searchCriteria.Keyword,
                     SearchFields = new[] { IndexDocumentExtensions.ContentFieldName },
                     Filter = filters.And(),
+                    Sorting = GetSorting(searchCriteria),
                     Skip = criteria.Skip,
                     Take = criteria.Take,
                 };
             }
 
-            return Task.FromResult(result);
+            return result;
         }
 
-        protected virtual IList<IFilter> GetFilters(ContentSearchCriteria criteria)
+        protected virtual async Task<IList<IFilter>> GetFilters(ContentSearchCriteria criteria)
         {
             var result = new List<IFilter>();
 
@@ -64,14 +68,8 @@ namespace VirtoCommerce.ContentModule.Data.Search
                 result.Add(CreateTermFilter("StoreId", criteria.StoreId));
             }
 
-            if (!string.IsNullOrEmpty(criteria.LanguageCode))
-            {
-                result.Add(CreateTermFilter("CultureName", criteria.LanguageCode));
-            }
-            if (!string.IsNullOrEmpty(criteria.CultureName))
-            {
-                result.Add(CreateTermFilter("CultureName", criteria.CultureName));
-            }
+            await AddLanguageFilter(criteria, result);
+
             if (!string.IsNullOrEmpty(criteria.FolderUrl))
             {
                 result.Add(CreateTermFilter("FolderUrl", criteria.FolderUrl));
@@ -83,6 +81,34 @@ namespace VirtoCommerce.ContentModule.Data.Search
 
 
             return result;
+        }
+
+        private async Task AddLanguageFilter(ContentSearchCriteria criteria, List<IFilter> filter)
+        {
+            var cultureFilter = CreateTermFilter("CultureName", "any");
+            var useFilter = false;
+
+            if (!criteria.CultureName.IsNullOrEmpty())
+            {
+                useFilter = true;
+                cultureFilter = cultureFilter.Or(CreateTermFilter("CultureName", criteria.CultureName));
+            }
+            if (!criteria.LanguageCode.IsNullOrEmpty())
+            {
+                useFilter = true;
+                cultureFilter = cultureFilter.Or(CreateTermFilter("CultureName", criteria.LanguageCode));
+            }
+
+            if (useFilter)
+            {
+                var store = await _storeService.GetByIdAsync(criteria.StoreId);
+                var storeLanguage = store.DefaultLanguage;
+                if (!storeLanguage.IsNullOrEmpty())
+                {
+                    cultureFilter = cultureFilter.Or(CreateTermFilter("CultureName", storeLanguage));
+                }
+                filter.Add(cultureFilter);
+            }
         }
 
         protected virtual IList<SortingField> GetSorting(ContentSearchCriteria criteria)
