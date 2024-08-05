a1b833dc26636021e0641a1e2fdb99d947757060
Basil Kotov <basilkot@gmail.com>
2024-08-05 09:59:52 +0100

    send and handle messages about file state

---
+++ src/VirtoCommerce.ContentModule.Web/Scripts/services/files-draft.service.js
@@ -0,0 +1,28 @@
+angular.module('virtoCommerce.contentModule')
+    .service('virtoCommerce.contentModule.files-draft', [function () {
+
+        var $this = this;
+
+        this.getDraftFileName = function (blade) {
+            var relativeUrl = blade.currentEntity.relativeUrl;
+            // the draft page should be under editing in the designer
+
+            if (!relativeUrl.endsWith('-draft')) {
+                relativeUrl = relativeUrl + '-draft';
+            }
+
+            return relativeUrl;
+        };
+
+        this.undraftUrl = function(url) {
+            if (!!url && url.endsWith('-draft')) {
+                return url.substring(0, url.length - 6);
+            }
+            return url;
+        }
+
+
+        this.getTemplateKey = function (blade) {
+            return blade.contentType + '::' + $this.getDraftFileName(blade);
+        };
+    }])
