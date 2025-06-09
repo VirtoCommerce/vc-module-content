0d16fabd4a62175c4aeb1ae6521b622a982bfb2a
Eugeny Okhrimenko <Eugeny.Okhrimenko@virtoworks.com>
2025-06-09 11:25:06 +0200

    Add sql migration

--- src/VirtoCommerce.ContentModule.Data.SqlServer/Migrations/20250609092448_AddMenuAndMenuItemsAddMenuAndMenuItems.Designer.cs
+++ src/VirtoCommerce.ContentModule.Data.SqlServer/Migrations/20250609092448_AddMenuAndMenuItemsAddMenuAndMenuItems.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using VirtoCommerce.ContentModule.Data.Repositories;
 
@@ -11,9 +12,11 @@ using VirtoCommerce.ContentModule.Data.Repositories;
 namespace VirtoCommerce.ContentModule.Data.SqlServer.Migrations
 {
     [DbContext(typeof(MenuDbContext))]
-    partial class MenuDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250609092448_AddMenuAndMenuItemsAddMenuAndMenuItems")]
+    partial class AddMenuAndMenuItemsAddMenuAndMenuItems
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/VirtoCommerce.ContentModule.Data.SqlServer/Migrations/20250609092448_AddMenuAndMenuItemsAddMenuAndMenuItems.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VirtoCommerce.ContentModule.Data.SqlServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMenuAndMenuItemsAddMenuAndMenuItems : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ContentMenu",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(128)", maxLength: 128, nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StoreId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Language = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    OuterId = table.Column<string>(type: "nvarchar(128)", maxLength: 128, nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    CreatedBy = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: true),
+                    ModifiedBy = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ContentMenu", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ContentMenuItem",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(128)", maxLength: 128, nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Url = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Priority = table.Column<int>(type: "int", nullable: false),
+                    AssociatedObjectId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AssociatedObjectName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AssociatedObjectType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    OuterId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MenuId = table.Column<string>(type: "nvarchar(128)", nullable: true),
+                    ParentMenuItemId = table.Column<string>(type: "nvarchar(128)", nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    CreatedBy = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: true),
+                    ModifiedBy = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ContentMenuItem", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ContentMenuItem_ContentMenuItem_ParentMenuItemId",
+                        column: x => x.ParentMenuItemId,
+                        principalTable: "ContentMenuItem",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ContentMenuItem_ContentMenu_MenuId",
+                        column: x => x.MenuId,
+                        principalTable: "ContentMenu",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ContentMenuItem_MenuId",
+                table: "ContentMenuItem",
+                column: "MenuId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ContentMenuItem_ParentMenuItemId",
+                table: "ContentMenuItem",
+                column: "ParentMenuItemId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ContentMenuItem");
+
+            migrationBuilder.DropTable(
+                name: "ContentMenu");
+        }
+    }
+}
